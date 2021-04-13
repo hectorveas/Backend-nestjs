90fb5521cf60f00c15db0424d82ebebbda5eb5f5
Hector Veas <hveas10@gmail.com>
2021-04-13 02:33:16 -0400

    Se agregaron peticiones y dependencias

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { Task } from './interfaces/Task';
+
+@Injectable()
+export class TasksService {
+
+    constructor(@InjectModel('Task') private taskModel: Model<Task>) {}
+
+
+    async getTasks() {
+        return await this.taskModel.find();
+    }
+
+    async getTask(id: string){
+        return await this.taskModel.findById(id);
+    }
+}
